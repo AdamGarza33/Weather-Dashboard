6b59519deb8457040b730c63c417ffb90e457c04
Addit <adam.garza33@hotmail.com>
2023-03-08 18:19:13 -0600

    search city funtion and added API key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Store seached city
+// Store searched city
 var city = "";
 
 // Declaration
@@ -10,3 +10,16 @@ var currentTempEl = $('#temp');
 var currentWindEl = $('#wind');
 var currentHumidityEl = $('#humid');
 var xCity = [];
+
+// search city
+function find(x) {
+    for (var i = 0; i < xCity.length; i++) {
+        if (x.toUpperCase() === xCity[i]) {
+            return -1;
+        }
+    }
+    return 1;
+}
+
+// API Key
+var APIKey = "94db56ea5c188e040498ca253c22154b";
