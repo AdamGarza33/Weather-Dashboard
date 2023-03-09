c42f970d0c22e6b0a13959f26c5cd5ba0a8f5fc4
Addit <adam.garza33@hotmail.com>
2023-03-08 18:15:05 -0600

    added var for search city and added var declarations

---
+++ script.js
@@ -0,0 +1,12 @@
+// Store seached city
+var city = "";
+
+// Declaration
+var findCity = $('#search-city');
+var searchBtn = $('#search-button');
+var clearSavedCityBtn = $('#clear-cities');
+var currentCityEl = $('#current-city');
+var currentTempEl = $('#temp');
+var currentWindEl = $('#wind');
+var currentHumidityEl = $('#humid');
+var xCity = [];
